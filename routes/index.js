199229ca23647d9d5cb5613fdc5e6039f47fbf3c
Lucas Ferrer <ferrer.xtreme@gmail.com>
2019-04-24 11:39:04 -0300

    Melhorias no layout

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ var passport = require('passport');
 
 /* GET home page. */
 
+// TITLE DO SITE
+
+const title = "Get Set Pet";
 
 
 // ROTA DE CONTROLE DE LOGIN - LOGOUT
@@ -11,6 +14,7 @@ var passport = require('passport');
 function authenticationMiddleware () {  
   return function (req, res, next) {
     if (req.isAuthenticated()) {
+      console.log(req.session.passport.user)
       return next()
     }
     // return next()
@@ -20,11 +24,11 @@ function authenticationMiddleware () {
 
 router.get('/login', function(req, res){
   if(req.query.fail)
-    res.render('login',{ title: 'Login', message: 'Usuário e/ou senha incorretos!' });
+    res.render('login',{ title: title + ' Login', message: 'Usuário e/ou senha incorretos!' });
   if(req.query.notLogged)
-    res.render('login',{ title: 'Login', message: 'Por gentileza realize o login para acessar o sistema!' });
+    res.render('login',{ title: title + ' Login', message: 'Por gentileza realize o login para acessar o sistema!' });
   else
-    res.render('login', { title: 'Login', message: "" });
+    res.render('login', { title: title + ' Login', message: "" });
 });
 
 router.post('/login',
@@ -38,35 +42,35 @@ router.get('/logout', function(req, res){
 // ROTAS PAGINAS
 
 router.get('/', function(req, res, next) {
-  res.render('public', { title: 'Projeto PI2' });
+  res.render('public', { title: title });
 });
 router.get('/index', authenticationMiddleware(), function(req, res, next) {
-  res.render('index', { title: 'Projeto PI2' });
+  res.render('index', { title: title , username: req.session.passport.user});
 });
 router.get('/charts', authenticationMiddleware(), function(req, res, next) {
-  res.render('charts', { title: 'Charts PI2' });
+  res.render('charts', { title: title , username: req.session.passport.user });
 });
 
 router.get('/forgot', function(req, res, next) {
-  res.render('forgot-password', { title: 'Esqueceu a senha PI2' });
+  res.render('forgot-password', { title: title });
 });
 router.get('/register', authenticationMiddleware(), function(req, res, next) {
-  res.render('register', { title: 'Registrar PI2' });
+  res.render('register', { title: title , username: req.session.passport.user });
 });
 router.get('/tables', authenticationMiddleware(), function(req, res, next) {
-  res.render('tables', { title: 'Tabelas PI2' });
+  res.render('tables', { title: title , username: req.session.passport.user });
 });
 router.get('/cards', authenticationMiddleware(), function(req, res, next) {
-  res.render('cards', { title: 'Cards PI2' });
+  res.render('cards', { title: title , username: req.session.passport.user });
 });
 router.get('/utilities-color', authenticationMiddleware(), function(req, res, next) {
-  res.render('utilities-color', { title: 'Utilities PI2' });
+  res.render('utilities-color', { title: title , username: req.session.passport.user });
 });
 router.get('/utilities-animation', authenticationMiddleware(), function(req, res, next) {
-  res.render('utilities-animation', { title: 'Utilities PI2' });
+  res.render('utilities-animation', { title: title , username: req.session.passport.user });
 });
 router.get('/utilities-other', authenticationMiddleware(), function(req, res, next) {
-  res.render('utilities-other', { title: 'Utilities PI2' });
+  res.render('utilities-other', { title: title , username: req.session.passport.user });
 });
 
 module.exports = router;
